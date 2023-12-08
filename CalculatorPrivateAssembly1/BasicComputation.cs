3d25347cc1d1660688b38d2d68795f1fa9195189
Crexur <106311200+Crexur@users.noreply.github.com>
2023-12-08 11:01:45 +0800

    PrivateAssembly

---
+++ CalculatorPrivateAssembly1/BasicComputation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorPrivateAssembly1
+{
+    public class BasicComputation
+    {
+        public static float Addition(float number1, float number2)
+        {
+            return number1 + number2;
+        }
+
+        public static float Subtraction(float number1, float number2)
+        {
+            return number1 - number2;
+        }
+
+        public static float Multiplication(float number1, float number2)
+        {
+            return number1 * number2;
+        }
+
+        public static float Division(float number1, float number2)
+        {
+            if (number2 != 0)
+                return number1 / number2;
+            else
+                throw new DivideByZeroException("Cannot divide by zero.");
+        }
+
+    }
+}
